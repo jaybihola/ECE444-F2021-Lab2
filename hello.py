b614aea5ccb5276e85611eb39ead72750e127d55
Jay Bihola <jaybih@outlook.com>
2021-09-26 20:56:49 -0400

    Added email field with checking to only allow utoronto emails

--- hello.py
+++ hello.py
@@ -3,7 +3,7 @@ from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from flask_wtf import Form
 from wtforms import StringField, SubmitField
-from wtforms.validators import Required
+from wtforms.validators import Required, Email
 
 from datetime import datetime
 
@@ -18,20 +18,34 @@ moment = Moment(app)
 
 class NameForm(Form):
     name = StringField('What is your name?', validators=[Required()])
+    email = StringField("What is your UofT Email Address?", validators=[Required(), Email(), ])
     submit = SubmitField('Submit')
 
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     name = None
+    email = None
     form = NameForm()
     if form.validate_on_submit():
         old_name = session.get('name')
         if old_name is not None and old_name != form.name.data:
             flash('Looks like you have changed your name!')
         session['name'] = form.name.data
+        old_email = session.get('email') 
+        if old_email is not None and old_email != form.email.data:
+            flash('Looks like you have changed your email!')
+
+        if "utoronto" in form.email.data.lower():
+            if old_email is not None and old_email != form.email.data:
+                flash('Looks like you have changed your email!')
+            session['email'] = form.email.data
+        else:
+            session['email'] = "invalid"
+
+
         return redirect(url_for('index'))
-    return render_template('index.html', form=form, name=session.get('name'))
+    return render_template('index.html', form=form, name=session.get('name'), email=session.get('email'))
 
 
 @app.route('/user/<name>')
